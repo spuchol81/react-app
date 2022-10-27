9d9e725ed86bd7672eb95f5aa3c7a63083d87874
spuchol81 <spuchol@vmware.com>
2022-10-27 13:54:59 +0200

    Align

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import reactlogo from './logo.svg';
 import logo from './heart.svg';
+import Tanzulogo from './logo-vmware.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="background">
-        VMware Tanzu
-        <img src={logo} className="App-logo" alt="logo" /> 
-        NodeJS
+      <div className="App-header"><img className="App" src={Tanzulogo} alt="Tanzulogo" /> + <img  className="React-logo" src={reactlogo} alt="reactlogo" width="30%" height="30%"/> = <img src={logo} className="App-logo" alt="logo" /></div>
         <a
           className="App-link"
           href="https://reactjs.org"
